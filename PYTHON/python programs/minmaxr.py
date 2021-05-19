0b1e93de5f2923af2a513dd7df12c6736d258995
wasimk739 <wasimk739@gmail.com>
2021-05-20 03:23:53 +0530

    python program to find min max from a random array

--- PYTHON/python programs/minmaxr.py
+++ PYTHON/python programs/minmaxr.py
@@ -1,7 +1,7 @@
 import random
 def minmax1(x):
     # this function fails if the list length is 0
-    minimum = maximum = x[0]
+    minimum, maximum = x[0], x[0]
     for i in x[1:]:
         if i < minimum:
             minimum = i
