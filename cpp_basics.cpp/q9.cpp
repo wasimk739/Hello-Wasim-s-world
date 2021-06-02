7883705f94114706e70ce6a4f8b33b5fc382456a
wasimk739 <wasimk739@gmail.com>
2021-06-03 02:30:06 +0530

    Write a program in C++ to display the operation of pre and post increment and decrement.

---
+++ cpp_basics.cpp/q9.cpp
@@ -0,0 +1,21 @@
+// Write a program in C++ to display the operation of pre and post increment and decrement.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=57;
+    a++;
+    cout<<"post increment the value of a by 1 is : "<<a<<endl;
+    ++a;
+    cout<<"pre increment the value of a by 1 is : "<<a<<endl;
+    a=a+1;
+    cout<<"after increasing the value of a by 1 is : "<<a<<endl;
+    a--;
+    cout<<"post decrement the value of a by 1 is : "<<a<<endl;
+    --a;
+    cout<<"pre decrement the value of a by 1is : "<<a<<endl;
+    a=a-1;
+    cout<<"after decreasing the value of a by 1 is : "<<a<<endl;
+    return 0;
+}
