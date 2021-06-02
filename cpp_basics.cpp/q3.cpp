3e0afd3f42dc118c427027c32c3ffc560ba2bd04
wasimk739 <wasimk739@gmail.com>
2021-06-03 01:14:52 +0530

    Write a program in C++ to find Size of fundamental data types.

---
+++ cpp_basics.cpp/q3.cpp
@@ -0,0 +1,18 @@
+// Write a program in C++ to find Size of fundamental data types. Go to the editor
+// Sample Output:
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"the size of char is:"<<sizeof(char)<<" bytes"<<endl;
+    cout<<"the size of short is:"<<sizeof(short)<<" bytes"<<endl;
+    cout<<"the size of int is:"<<sizeof(int)<<" bytes"<<endl;
+    cout<<"the size of long is:"<<sizeof(long)<<" bytes"<<endl;
+    cout<<"the size of long long is:"<<sizeof(long long)<<" bytes"<<endl;
+    cout<<"the size of float is:"<<sizeof(float)<<" bytes"<<endl;
+    cout<<"the size of doouble is:"<<sizeof(double)<<" bytes"<<endl;
+    cout<<"the size of long double is:"<<sizeof(long double)<<" bytes"<<endl;
+    cout<<"the size of bool is:"<<sizeof(bool)<<" bytes"<<endl;
+    return 0;
+}
