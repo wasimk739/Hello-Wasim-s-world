c826ca06648bd1a40d602aab77380de0a2a9b7d1
wasimk739 <wasimk739@gmail.com>
2021-06-03 01:08:25 +0530

    Write a program in C++ to print the sum of two numbers

---
+++ cpp_basics.cpp/q2.cpp
@@ -0,0 +1,14 @@
+// Write a program in C++ to print the sum of two numbers. Go to the editor
+// Sample Output:
+// Print the sum of two numbers :
+// -----------------------------------
+// The sum of 29 and 30 is : 59
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"the sum of 29 and 30 is ::"<<29+30<<endl;
+    return 0;
+}
