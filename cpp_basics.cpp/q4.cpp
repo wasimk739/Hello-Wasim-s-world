850510d74a887e755558226f8f7fe539e68878af
wasimk739 <wasimk739@gmail.com>
2021-06-03 01:33:18 +0530

    Write a program in C++ to print the sum of two numbers using variables

---
+++ cpp_basics.cpp/q4.cpp
@@ -0,0 +1,13 @@
+// Write a program in C++ to print the sum of two numbers using variables
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    a=29;
+    b=30;
+    c=a+b;
+    cout<<"the sum of 29 and 30 is : "<<c<<endl;
+    return 0;
+}
